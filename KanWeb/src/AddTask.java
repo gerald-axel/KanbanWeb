87db7198c58163a996144e1ea18f642758e03373
Gerald Axel <Gerald Axel@Gerald>
2014-06-02 09:05:38 -0500

    Doget, dopost

---
+++ KanWeb/src/AddTask.java
@@ -0,0 +1,95 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddTask extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter writer= response.getWriter();
+		writer.print("<html>"
+				+ "<head>"
+				+"<title>Add Task</title>"
+				+ "</head>"
+				+"<body>"
+				+"<form method=\"POST\">"
+				+"<table>"
+				+"<tr>"
+				+"<td>Tittle:</td>"
+				+"<td>"
+				+"<input type=\"text\" name=\"title\"/>"
+				+"</td>"
+				+"</tr>"
+				
+				+"<tr>"
+				+"<td>Description:</td>"
+				+"<td>"
+				+"<input type=\"text\" name=\"description\"/>"
+				+"</td>"
+				+"</tr>"
+				
+				+"<tr>"
+				+"<td>Category:</td>"
+				+"<td>"
+				+"<input type=\"text\" name=\"category\"/>"
+				+"</td>"
+				+"</tr>"
+				
+
+				+"<tr>"
+				+"<td>Owner:</td>"
+				+"<td>"
+				+"<input type=\"text\" name=\"owner\"/>"
+				+"</td>"
+				+"</tr>"
+				
+				+"<tr>"
+				+"<td>State:</td>"
+				+"<td>"
+				+"<select>"
+				+"<option value=\"BACKLOG\"selected>Backlog</option>"
+				+"<option value=\"TO_DO\">To do</option>"
+				+"<option value=\"IN_PROGRESS\">In progress</option>"
+				+"<option value=\"DONE\">done</option>"
+				+"</select>"	
+				+"</td>"
+				+"</tr>"
+				
+				+"<tr>"
+				+"<td>Due Date:</td>"
+				+"<td>"
+				+"<input type=\"text\" name=\"category\"/>"
+				+"</td>"
+				+"</tr>"
+				
+				+"<tr>"
+				+"<td colspan=\"2\">"
+				+"<input type=\"submit\" value=\"Add\"/>"
+				+"</td>"
+				+"<tr>"
+
+
+				+"</table>"
+				+"</form>"
+				+"</body>"
+				+"</html>");
+	} 
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException{
+		String title=request.getParameter("title");
+				
+				response.setContentType("text/html");
+		PrintWriter writer= response.getWriter();
+		writer.print("<html>"
+				+"<body>"
+				+"Title: "+title
+				+"</body>"
+				+"</html>");
+	}
+
+}
